d5de59e08437781d30a01f858907a07ad9fa0ac0
zzn0824@126.com <zzn0824@126.com>
2022-01-01 18:54:58 +0800

    3.位置值设置为可变化的值

--- src/com/pazhu/tank/Main.java
+++ src/com/pazhu/tank/Main.java
@@ -1,10 +1,6 @@
 package com.pazhu.tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class T {
+public class Main {
     public static void main(String[] args) {
         TankFrame tankFrame = new TankFrame();
     }
--- src/com/pazhu/tank/TankFrame.java
+++ src/com/pazhu/tank/TankFrame.java
@@ -6,22 +6,26 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
-      public TankFrame() {
-          setSize(800,600);
-          setVisible(true);
-          setResizable(false);
-          setTitle("tank game");
-          addWindowListener(new WindowAdapter() {
-              @Override
-              public void windowClosing(WindowEvent e) {
-                  System.exit(0);
-              }
-          });
-      }
+    int x = 20,y=20;
+
+    public TankFrame() {
+      setSize(800,600);
+      setVisible(true);
+      setResizable(false);
+      setTitle("tank game");
+      addWindowListener(new WindowAdapter() {
+          @Override
+          public void windowClosing(WindowEvent e) {
+              System.exit(0);
+          }
+      });
+    }
 
     @Override
     public void paint(Graphics g) {
-          g.fillRect(200,200,35,35);
+          g.fillRect(x,y,35,35);
+          x+=5;
+          y+=5;
     }
 
 }
